86b711f18c5ff300f447da0f05ea520f32e8e3df
MaulanaRhezaArdiansyah <rhezaardiansyah222@gmail.com>
2022-06-10 23:05:09 +0800

    membuat website farez

--- main.js
+++ main.js
@@ -27,32 +27,32 @@ function scrollHeader() {
 }
 window.addEventListener('scroll', scrollHeader);
 
-/*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'bx-sun';
+// /*==================== DARK LIGHT THEME ====================*/
+// const themeButton = document.getElementById('theme-button');
+// const lightTheme = 'light-theme';
+// const iconTheme = 'bx-moon';
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
+// // Previously selected topic (if user selected)
+// const selectedTheme = localStorage.getItem('selected-theme');
+// const selectedIcon = localStorage.getItem('selected-icon');
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
-const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun');
+// // We obtain the current theme that the interface has by validating the light-theme class
+// const getCurrentTheme = () => (document.body.classList.contains(lightTheme) ? 'light' : 'dark');
+// const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'bx-sun' : 'bx-moon');
 
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
-}
+// // We validate if the user previously chose a topic
+// if (selectedTheme) {
+//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+//   document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](lightTheme);
+//   themeButton.classList[selectedIcon === 'bx-sun' ? 'add' : 'remove'](iconTheme);
+// }
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem('selected-theme', getCurrentTheme());
-  localStorage.setItem('selected-icon', getCurrentIcon());
-});
+// // Activate / deactivate the theme manually with the button
+// themeButton.addEventListener('click', () => {
+//   // Add or remove the light / icon theme
+//   document.body.classList.toggle(lightTheme);
+//   themeButton.classList.toggle(iconTheme);
+//   // We save the theme and the current icon that the user chose
+//   localStorage.setItem('selected-theme', getCurrentTheme());
+//   localStorage.setItem('selected-icon', getCurrentIcon());
+// });
